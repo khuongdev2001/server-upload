af6bafc0f630937b3b8e6213e4a4a42657abf45d
Your Name <you@example.com>
2024-06-08 22:39:33 +0700

    add filter phone and convert general format column excel

--- index.php
+++ index.php
@@ -9,12 +9,14 @@ header("Content-Type:application/json");
 switch ($_SERVER["REQUEST_URI"]) {
     case "/api/v1/read-excel":
         $inputFileName = $_FILES["file"]["tmp_name"];
-        // /** Load $inputFileName to a Spreadsheet Object  **/
+        /** Load $inputFileName to a Spreadsheet Object **/
         $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
         $worksheet = $spreadsheet->getActiveSheet();
         $result = [];
         $time = time();
-        foreach ($worksheet->toArray() as $index => $row) {
+
+        $response["data"] = $worksheet->toArray();
+        foreach ($worksheet->toArray(null, false, false) as $index => $row) {
             if ($index == 0) {
                 continue;
             }
@@ -23,29 +25,38 @@ switch ($_SERVER["REQUEST_URI"]) {
             }
             $fullname = $row[0];
             $phones = preg_split("/[., ;|]/", $row[2], -1, PREG_SPLIT_NO_EMPTY);
-            foreach($phones as $phone){
+            foreach ($phones as $phone) {
                 $result[] = [
                     "id" => $time + $index,
                     "fullname" => handleText($fullname),
-                    "phone" => handleText($phone)
+                    "phone" => filterPhone(handleText($phone))
                 ];
             }
         }
-        if(!empty($_POST["remove_duplicate"])){
+        if (!empty($_POST["remove_duplicate"])) {
             $result = uniqueContacts($result, "phone");
         }
         $response["data"] = $result;
         break;
     default:
-        
 }
 
-function handleText($str){
-    if(empty($_POST["is_slug"])){
+function handleText($str)
+{
+    if (empty($_POST["is_slug"])) {
         return $str;
     }
     return slug($str);
 }
+
+
+function filterPhone($phone){
+    if(strlen($phone) < 10){
+      return "0".$phone;
+    }
+ 
+    return $phone;
+ }
 
 function uniqueContacts($contacts, $fieldCheck)
 {
@@ -57,32 +68,37 @@ function uniqueContacts($contacts, $fieldCheck)
     return array_values($contactFiltered);
 }
 
-function slug($str) {
+function slug($str)
+{
     return str_replace(
-        array('á', 'à', 'ả', 'ạ', 'ã', 'ă', 'ắ', 'ằ', 'ẳ', 'ẵ', 'ặ', 'â', 'ấ', 'ầ', 'ẩ', 'ẫ', 'ậ',
-              'Á', 'À', 'Ả', 'Ạ', 'Ã', 'Ă', 'Ắ', 'Ằ', 'Ẳ', 'Ẵ', 'Ặ', 'Â', 'Ấ', 'Ầ', 'Ẩ', 'Ẫ', 'Ậ',
-              'é', 'è', 'ẻ', 'ẽ', 'ẹ', 'ê', 'ế', 'ề', 'ể', 'ễ', 'ệ',
-              'É', 'È', 'Ẻ', 'Ẽ', 'Ẹ', 'Ê', 'Ế', 'Ề', 'Ể', 'Ễ', 'Ệ',
-              'í', 'ì', 'ỉ', 'ĩ', 'ị',
-              'Í', 'Ì', 'Ỉ', 'Ĩ', 'Ị',
-              'ó', 'ò', 'ỏ', 'õ', 'ọ', 'ô', 'ố', 'ồ', 'ổ', 'ỗ', 'ộ', 'ơ', 'ớ', 'ờ', 'ở', 'ỡ', 'ợ',
-              'Ó', 'Ò', 'Ỏ', 'Õ', 'Ọ', 'Ô', 'Ố', 'Ồ', 'Ổ', 'Ỗ', 'Ộ', 'Ơ', 'Ớ', 'Ờ', 'Ở', 'Ỡ', 'Ợ',
-              'ú', 'ù', 'ủ', 'ũ', 'ụ', 'ư', 'ứ', 'ừ', 'ử', 'ữ', 'ự',
-              'Ú', 'Ù', 'Ủ', 'Ũ', 'Ụ', 'Ư', 'Ứ', 'Ừ', 'Ử', 'Ữ', 'Ự',
-              'Ý', 'Ỳ', 'Ỷ', 'Ỹ', 'Ỵ',
-              'đ', 'Đ'),
-        array('a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',
-              'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
-              'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e',
-              'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
-              'i', 'i', 'i', 'i', 'i',
-              'I', 'I', 'I', 'I', 'I',
-              'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',
-              'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
-              'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u',
-              'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U',
-              'Y', 'Y', 'Y', 'Y', 'Y',
-              'd', 'D'),
+        array(
+            'á', 'à', 'ả', 'ạ', 'ã', 'ă', 'ắ', 'ằ', 'ẳ', 'ẵ', 'ặ', 'â', 'ấ', 'ầ', 'ẩ', 'ẫ', 'ậ',
+            'Á', 'À', 'Ả', 'Ạ', 'Ã', 'Ă', 'Ắ', 'Ằ', 'Ẳ', 'Ẵ', 'Ặ', 'Â', 'Ấ', 'Ầ', 'Ẩ', 'Ẫ', 'Ậ',
+            'é', 'è', 'ẻ', 'ẽ', 'ẹ', 'ê', 'ế', 'ề', 'ể', 'ễ', 'ệ',
+            'É', 'È', 'Ẻ', 'Ẽ', 'Ẹ', 'Ê', 'Ế', 'Ề', 'Ể', 'Ễ', 'Ệ',
+            'í', 'ì', 'ỉ', 'ĩ', 'ị',
+            'Í', 'Ì', 'Ỉ', 'Ĩ', 'Ị',
+            'ó', 'ò', 'ỏ', 'õ', 'ọ', 'ô', 'ố', 'ồ', 'ổ', 'ỗ', 'ộ', 'ơ', 'ớ', 'ờ', 'ở', 'ỡ', 'ợ',
+            'Ó', 'Ò', 'Ỏ', 'Õ', 'Ọ', 'Ô', 'Ố', 'Ồ', 'Ổ', 'Ỗ', 'Ộ', 'Ơ', 'Ớ', 'Ờ', 'Ở', 'Ỡ', 'Ợ',
+            'ú', 'ù', 'ủ', 'ũ', 'ụ', 'ư', 'ứ', 'ừ', 'ử', 'ữ', 'ự',
+            'Ú', 'Ù', 'Ủ', 'Ũ', 'Ụ', 'Ư', 'Ứ', 'Ừ', 'Ử', 'Ữ', 'Ự',
+            'Ý', 'Ỳ', 'Ỷ', 'Ỹ', 'Ỵ',
+            'đ', 'Đ'
+        ),
+        array(
+            'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',
+            'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
+            'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e',
+            'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
+            'i', 'i', 'i', 'i', 'i',
+            'I', 'I', 'I', 'I', 'I',
+            'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',
+            'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+            'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u',
+            'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U',
+            'Y', 'Y', 'Y', 'Y', 'Y',
+            'd', 'D'
+        ),
         $str
     );
 }
